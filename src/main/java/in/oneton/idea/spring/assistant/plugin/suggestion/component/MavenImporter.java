d99e4c4369746f2e4e07aee8cd5117adaf60ce51
Cekerel <zt1335204909@gmail.com>
2025-02-28 11:30:17 +0800

    feat: Support version 243.* and remove deprecated Class using

--- src/main/java/in/oneton/idea/spring/assistant/plugin/suggestion/component/MavenImporter.java
+++ src/main/java/in/oneton/idea/spring/assistant/plugin/suggestion/component/MavenImporter.java
@@ -1,46 +1,37 @@
 package in.oneton.idea.spring.assistant.plugin.suggestion.component;
 
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
-import com.intellij.openapi.module.Module;
-import org.jetbrains.idea.maven.importing.MavenRootModelAdapter;
-import org.jetbrains.idea.maven.project.MavenProject;
-import org.jetbrains.idea.maven.project.MavenProjectChanges;
-import org.jetbrains.idea.maven.project.MavenProjectsProcessorTask;
-import org.jetbrains.idea.maven.project.MavenProjectsTree;
+import org.jetbrains.idea.maven.importing.MavenWorkspaceConfigurator;
 
-import java.util.List;
-import java.util.Map;
-
-public class MavenImporter extends org.jetbrains.idea.maven.importing.MavenImporter {
+public class MavenImporter implements MavenWorkspaceConfigurator {
 
   private static final Logger log = Logger.getInstance(MavenProcessorTask.class);
 
   public MavenImporter(String pluginGroupID, String pluginArtifactID) {
-    super(pluginGroupID, pluginArtifactID);
+    super();
   }
 
-  @Override
-  public void preProcess(Module module, MavenProject mavenProject,
-      MavenProjectChanges mavenProjectChanges,
-      IdeModifiableModelsProvider ideModifiableModelsProvider) {
-
-  }
-
-  @Override
-  public void process(IdeModifiableModelsProvider ideModifiableModelsProvider, Module module,
-      MavenRootModelAdapter mavenRootModelAdapter, MavenProjectsTree mavenProjectsTree,
-      MavenProject mavenProject, MavenProjectChanges mavenProjectChanges,
-      Map<MavenProject, String> map, List<MavenProjectsProcessorTask> processorTasks) {
-    String skip = this.findConfigValue(mavenProject, "skip");
-    if (!"true".equals(skip)) {
-      processorTasks.add(new MavenProcessorTask(module));
-    } else {
-      debug(() -> log.debug(
-          "Skipping index check for project " + module.getProject().getName() + " & module "
-              + module.getName()));
-    }
-  }
+//  @Override
+//  public void preProcess(Module module, MavenProject mavenProject,
+//      MavenProjectChanges mavenProjectChanges,
+//      IdeModifiableModelsProvider ideModifiableModelsProvider) {
+//
+//  }
+//
+//  @Override
+//  public void process(IdeModifiableModelsProvider ideModifiableModelsProvider, Module module,
+//      MavenRootModelAdapter mavenRootModelAdapter, MavenProjectsTree mavenProjectsTree,
+//      MavenProject mavenProject, MavenProjectChanges mavenProjectChanges,
+//      Map<MavenProject, String> map, List<MavenProjectsProcessorTask> processorTasks) {
+//    String skip = this.findConfigValue(mavenProject, "skip");
+//    if (!"true".equals(skip)) {
+//      processorTasks.add(new MavenProcessorTask(module));
+//    } else {
+//      debug(() -> log.debug(
+//          "Skipping index check for project " + module.getProject().getName() + " & module "
+//              + module.getName()));
+//    }
+//  }
 
   /**
    * Debug logging can be enabled by adding fully classified class name/package name with # prefix
